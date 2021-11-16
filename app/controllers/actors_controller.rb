88596ed32e0aafdab915168b6e320f1fe837c8a7
anonymous anonymous <shashwatssinha@gmail.com>
2021-11-16 17:49:07 +0000

    Remove controller comments

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,24 +1,19 @@
 class ActorsController < ApplicationController
   before_action :set_actor, only: %i[show edit update destroy]
 
-  # GET /actors
   def index
     @q = Actor.ransack(params[:q])
     @actors = @q.result(distinct: true).includes(:starred_in).page(params[:page]).per(10)
   end
 
-  # GET /actors/1
   def show; end
 
-  # GET /actors/new
   def new
     @actor = Actor.new
   end
 
-  # GET /actors/1/edit
   def edit; end
 
-  # POST /actors
   def create
     @actor = Actor.new(actor_params)
 
@@ -34,7 +29,6 @@ class ActorsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /actors/1
   def update
     if @actor.update(actor_params)
       redirect_to @actor, notice: "Actor was successfully updated."
@@ -43,7 +37,6 @@ class ActorsController < ApplicationController
     end
   end
 
-  # DELETE /actors/1
   def destroy
     @actor.destroy
     message = "Actor was successfully deleted."
@@ -56,12 +49,10 @@ class ActorsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_actor
     @actor = Actor.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def actor_params
     params.require(:actor).permit(:actor_name, :starred_in_id, :actor_dob,
                                   :actor_bio, :actor_image)
